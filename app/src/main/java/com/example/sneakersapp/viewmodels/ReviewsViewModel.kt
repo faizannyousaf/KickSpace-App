7a7eec4d5e477f6352778896d0d1d536c5bb62e1
Faizan Yousaf <faizanyousaff1@gmail.com>
2025-10-13 09:37:39 +0100

    Implementing DI Currently on coroutines.

---
+++ app/src/main/java/com/example/sneakersapp/viewmodels/ReviewsViewModel.kt
@@ -0,0 +1,23 @@
+package com.example.sneakersapp.viewmodels
+
+import android.view.View
+import androidx.lifecycle.ViewModel
+import com.example.sneakersapp.model.entities.Review
+import com.example.sneakersapp.model.repositories.ReviewsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+
+@HiltViewModel
+class ReviewsViewModel @Inject constructor(
+                       private val repository : ReviewsRepository) : ViewModel() {
+
+   suspend fun insertDummyData(){
+        repository.insertReviews(Review(1,"very good shoes",
+            2,3,3))
+    }
+
+    suspend fun fetchReviews() : List<Review>{
+       return repository.fetchReviews()
+    }
+}
