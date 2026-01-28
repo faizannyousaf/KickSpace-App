62f8eb6b1e6e47f6bc85f10f7b60856be4afaeb8
Faizan Yousaf <faizanyousaff1@gmail.com>
2026-01-28 11:41:05 +0000

    JSON data added instead of dummy data.

--- app/src/main/java/com/example/sneakersapp/model/repositories/SneakerRepository.kt
+++ app/src/main/java/com/example/sneakersapp/model/repositories/SneakerRepository.kt
@@ -11,34 +11,26 @@ import javax.inject.Inject
 
 class SneakerRepository @Inject constructor(
     private val sneakerDao: SneakerDao,
-    @ApplicationContext private val context: Context // Add this annotation!
+    @ApplicationContext private val context: Context
 ) {
 
 
     fun fetchSneakers(): Flow<List<Sneaker>> = sneakerDao.fetchSneakers()
 
-//    suspend fun insertSneaker(sneaker: Sneaker) {
-//        sneakerDao.insertSneaker(sneaker)
-//    }
     suspend fun checkAndSeedDatabase() {
 
-            // 2. Read JSON from assets
+            //  Read JSON from assets
             val jsonString = context.assets.open("sneaker_starter_pack.json")
                 .bufferedReader().use { it.readText() }
 
-            // 3. Parse JSON to List (using Gson)
+           // Parse JSON to List (using Gson)
             val type = object : TypeToken<List<Sneaker>>() {}.type
             val list: List<Sneaker> = Gson().fromJson(jsonString, type)
 
-            // 4. Insert into Room
+            //  Insert into Room
             sneakerDao.insertAllSneakers(list)
 
     }
-
-
-//    suspend fun insertAllSneakers(sneakers: List<Sneaker>){
-//        sneakerDao.insertAllSneakers(sneakers)
-//    }
 
     suspend fun deleteSneaker(sneaker: Sneaker){
         sneakerDao.deleteSneaker(sneaker)
